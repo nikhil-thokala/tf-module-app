81538a2d3db4d4077d8e4b579d9c87d4376e7272
nikhilt <nikhil.t1226@gmail.com>
2023-04-14 21:35:34 +0100

    \ |"?

--- vars.tf
+++ vars.tf
@@ -2,3 +2,7 @@ variable "env" {}
 variable "tags" {}
 variable "component" {}
 variable "instance_type" {}
+variable "desired_capacity" {}
+variable "min_size" {}
+variable "max_size" {}
+variable "subnets" {}
