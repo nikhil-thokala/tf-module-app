aa0e77218d1767ea1b00db2e5c7394d1390ffe90
nikhilt <nikhil.t1226@gmail.com>
2023-04-15 14:51:22 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -100,3 +100,20 @@ resource "aws_launch_template" "main" {
     ttl     = 30
     records = [var.alb_dns_name]
   }
+
+
+  resource "aws_lb_listener_rule" "listener_arn" {
+    listener_arn = var.listener_arn
+    priority     = var.listener_priority
+
+    action {
+      type             = "forward"
+      target_group_arn = aws_lb_target_group.main.arn
+    }
+
+    condition {
+      host_header {
+        values = [local.dns_name]
+      }
+    }
+  }
