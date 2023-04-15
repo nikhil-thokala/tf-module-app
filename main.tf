e031f6ae796f882e5a54cb2288dc66258edfe70d
nikhilt <nikhil.t1226@gmail.com>
2023-04-15 13:46:02 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ resource "aws_launch_template" "main" {
     max_size              = var.max_size
     min_size              = var.min_size
     vpc_zone_identifier   = var.subnets
+    target_group_arns     = [aws_lb_target_group.main.arn]
 
     launch_template {
       id      = aws_launch_template.main.id
@@ -77,3 +78,18 @@ resource "aws_launch_template" "main" {
 
   tags = merge(var.tags, { Name = "${var.component}- ${var.env}" })
 }
+
+    resource "aws_lb_target_group" "main" {
+      name     = "${var.component}- ${var.env}"
+      port     = var.port
+      protocol            = "HTTP"
+      vpc_id              = var.vpc_id
+      health_check {
+        healthy_threshold   = 2
+        unhealthy_threshold = 2
+        interval            = 5
+        timeout             = 4
+      }
+      tags = merge(var.tags, { Name = "${var.component}- ${var.env}" })
+
+    }
